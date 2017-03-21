4414339df772a167445feb77603189821f6d5ef2
Orhan Sönmez <orhnsnmz@gmail.com>
2017-03-21 22:32:29 +0100

    Added possibility to use Google's Geocoder on every request

--- js/geocoder.js
+++ js/geocoder.js
@@ -6,29 +6,37 @@ const { RNGeocoder } = NativeModules;
 export default {
   apiKey: null,
 
-  fallbackToGoogle(key) {
+  setGoogleApiKey(key) {
     this.apiKey = key;
   },
 
-  geocodePosition(position) {
+  geocodePosition(position, useGoogle = false) {
     if (!position || !position.lat || !position.lng) {
       return Promise.reject(new Error("invalid position: {lat, lng} required"));
     }
 
-    return RNGeocoder.geocodePosition(position).catch(err => {
-      if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
+    if (useGoogle && this.apiKey) {
       return GoogleApi.geocodePosition(this.apiKey, position);
-    });
+    } else {
+      return RNGeocoder.geocodePosition(position).catch(err => {
+        if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
+        return GoogleApi.geocodePosition(this.apiKey, position);
+      });
+    }
   },
 
-  geocodeAddress(address) {
+  geocodeAddress(address, useGoogle) {
     if (!address) {
       return Promise.reject(new Error("address is null"));
     }
 
-    return RNGeocoder.geocodeAddress(address).catch(err => {
-      if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
+    if (useGoogle && this.apiKey) {
       return GoogleApi.geocodeAddress(this.apiKey, address);
-    });
+    } else {
+      return RNGeocoder.geocodeAddress(address).catch(err => {
+        if (!this.apiKey || err.code !== 'NOT_AVAILABLE') { throw err; }
+        return GoogleApi.geocodeAddress(this.apiKey, address);
+      });
+    }
   },
 }
